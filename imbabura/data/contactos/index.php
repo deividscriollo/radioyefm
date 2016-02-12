198475aab76656bdbe9db6eb333842c33d00f868
natykhuera <natykhuera@hotmail.com>
2016-02-12 18:08:42 -0500

    Cambios7 mapas
    Realización del mapa para los contactos

--- imbabura/data/contactos/index.php
+++ imbabura/data/contactos/index.php
@@ -43,6 +43,9 @@
         <link id="link-footer-mode-r" href="../../css/mode/mode-1-footer-responsive.css" rel="stylesheet" type="text/css">   
     <!-- Custom CSS -->
     <link href="../../custom/custom.css" rel="stylesheet" type="text/css">
+    <!--Para los mapas-->
+    <link rel="stylesheet" href="leaflet.css" />
+    <script src="leaflet.js"></script>
     <!-- Head SCRIPTS -->
     <script type="text/javascript" src="../../js/modernizr.js"></script> 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -195,22 +198,33 @@
         </div>
         <!-- Fin del los  Items del Carousel -->
         <hr>
-          <br>
 <!-- Inicio de Filosofía -->
        <div class="col-md-12">
-            <h5 class="vc_widget-title text-center">Lugar</h5>
+            <h5 class="vc_widget-title text-center">Como LLegar</h5>
+<!--para el mapa-->
+                <div id="map" style="height: 400px; margin-top: 20px"></div>
+                <script>
+                  var greenIcon = L.icon({
+                    iconUrl: 'images/marker-icon.png',
+                    iconSize:     [38, 95], // size of the icon
+                    shadowSize:   [50, 64], // size of the shadow
+                    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+                    shadowAnchor: [4, 62],  // the same for the shadow
+                    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+                });
+                  var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                    osm = L.tileLayer(osmUrl, {maxZoom: 20, attribution: osmAttrib});
 
-  <div class="vc_map slide-waypoint block" data-waypoint="contact" style="margin-top:0; border-top:4px solid #CCCCCC;">
-    <div class="wrapper">
-      <div id="map" class="map"></div>
-    </div>
-    <!-- .wrapper --> 
-  </div>
-  <!-- .vc_map -->
+                  var map = L.map('map').setView([0.348742, -78.123154,12], 14).addLayer(osm);
+
+                  L.marker([0.348742, -78.123154,12], {icon: greenIcon}).addTo(map)
+                    .bindPopup('<div class="container"><a href="http://oyeecuador.com/"><img src="images/logo2.png"></a></div><p class="letras">Dir: Av. Mariano Acosta - Jaime Rivadeneira</p><p class="letras">Tlf: (06) 260-6027</p><p class="letras">Sitio Web: www.oyefm.com</p>');
+                      //.openPopup();
+                </script>
         </div>
 <!-- Fin del Mapa -->
 <!-- Inicio de los Integrantes -->
-
 <div class="col-md-12">
 <h5 class="vc_widget-title text-center">Corporativo Principal</h5>
        <div class="row text-center">
