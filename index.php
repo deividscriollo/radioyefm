53e9abf11ec32c15da7828720e37af5ae49778ff
Deivid <deividscriollo@gmail.com>
2016-02-16 12:09:44 -0500

    configuracio

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="dc criollo">
+    <link rel="shortcut icon" href="assets/img/favicon.png">
     <title>Oye, lo que te gusta</title>
     <!-- Bootstrap Core CSS -->
     <link href="assets/css/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css">
@@ -13,14 +14,6 @@
     <!-- Font Awesome -->
     <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <!-- Default Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,600,500,700,800,900' rel='stylesheet' type='text/css'>
-    <!-- Modern Style Fonts (Include these is you are using body.modern!) -->
-    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Cardo:400,400italic,700' rel='stylesheet' type='text/css'>
-    <!-- Vintage Style Fonts (Include these if you are using body.vintage!) -->
-    <link href='http://fonts.googleapis.com/css?family=Sanchez:400italic,400' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Cardo:400,400italic,700' rel='stylesheet' type='text/css'>
     <!-- Plugin CSS -->
     <link href="assets/css/plugins/owl-carousel/owl.carousel.css" rel="stylesheet" type="text/css">
     <link href="assets/css/plugins/owl-carousel/owl.theme.css" rel="stylesheet" type="text/css">
@@ -28,31 +21,8 @@
     <link href="assets/css/plugins/magnific-popup.css" rel="stylesheet" type="text/css">
     <link href="assets/css/plugins/background.css" rel="stylesheet" type="text/css">
     <link href="assets/css/plugins/animate.css" rel="stylesheet" type="text/css">
-    <!-- Vitality Theme CSS -->
-    <!-- Uncomment the color scheme you want to use. -->
-    <!-- <link href="assets/css/vitality-red.css" rel="stylesheet" type="text/css"> -->
-    <!-- <link href="assets/css/vitality-aqua.css" rel="stylesheet" type="text/css"> -->
-    <!-- <link href="assets/css/vitality-blue.css" rel="stylesheet" type="text/css"> -->
-    <link href="assets/css/vitality-green.css" rel="stylesheet" type="text/css">
-    <!-- <link href="assets/css/vitality-orange.css" rel="stylesheet" type="text/css"> -->
-    <!-- <link href="assets/css/vitality-pink.css" rel="stylesheet" type="text/css"> -->
-    <!-- <link href="assets/css/vitality-purple.css" rel="stylesheet" type="text/css"> -->
-    <!-- <link href="assets/css/vitality-tan.css" rel="stylesheet" type="text/css"> -->
-    <!-- <link href="assets/css/vitality-turquoise.css" rel="stylesheet" type="text/css"> -->
-    <!-- <link href="assets/css/vitality-yellow.css" rel="stylesheet" type="text/css"> -->
     <!-- Style Switcher CSS - Demo Purposes Only! -->
     <link id="changeable-colors" rel="stylesheet" href="assets/css/vitality-orange.css">
-    <link href="assets/demo/style-switcher.css" rel="stylesheet">
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-73179905-1', 'auto');
-      ga('send', 'pageview');
-
-    </script>
 </head>
 
 <body id="page-top">
@@ -135,12 +105,7 @@
     <script src="assets/js/plugins/jqBootstrapValidation.js"></script>
     <!-- Vitality Theme Scripts -->
     <script src="assets/js/vitality.js"></script>
-    <!-- Style Switcher Scripts - Demo Purposes Only! -->
-    <script src="assets/demo/style.switcher.js"></script>
 </body>
-
-
-<!-- Mirrored from themes.startbootstrap.com/vitality-v1.3.6/video.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 28 Dec 2015 21:42:42 GMT -->
 </html>
 
 
